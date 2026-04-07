a62a11d4a0047a9263211674d41703ec240fc59a
João Vitor Ramos Vitorino do Nascimento <joaovitorramosvitorino2011.10@gmail.com>
2026-04-06 23:33:25 -0300

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -1,1 +1,25 @@
+import mysql.connector
 
+def conectar():
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="monitoramento_agua"
+    )
+  
+    return conexao
+
+
+def obter_dados():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT valor FROM leituras")
+    dados = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    # transformar em lista simples
+    return [dado[0] for dado in dados]
